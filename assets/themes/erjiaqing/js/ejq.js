580444572d9919a9ae4d795ce27160c2151e62c3
Song Guo <gs199704@gmail.com>
2016-02-29 19:38:14 +0800

    Revert "ajax_load_test"
    This reverts commit dc8383a669888cf77f5045cafde9642b6df63a16.

--- assets/themes/erjiaqing/js/ejq.js
+++ assets/themes/erjiaqing/js/ejq.js
@@ -1,31 +1,3 @@
-function doAtReady() {
-	$(function () {
-	var res = "";
-	$("div#post-content h1, div#post-content h2, div#post-content h3, div#post-content h4, div#post-content h5, div#post-content h6").each(function(){
-		res = res + '<li><a href="#' + $(this).attr('id') + '">' + $(this).html() + '</a></li>';
-	});
-	$('#right-float').html($('#right-float').html() + '<section id="toc"><h4>ToC</h4><ul>' + res + '</ul>');
-	$('#right-float').stickUp();
-	});
-	$("a").click(function(event) {
-  	  jumpToNewPage(event.toElement.href);
-	  alert("jump");
-	  return false;
-    });
-}
-
-function jumpToNewPage(href) {
-  reg = new RegExp("<!--BEGIN_CONTENT-->(.|\\n)*<!--END_CONTENT-->", "gm");
-  $.get(href, function(data){$(".content").html(data.match(reg));});
-  history.pushState(location.href, null, href);
-  doAtReady();
-}
-
-window.onpopstate = function(event) {
-  reg = new RegExp("<!--BEGIN_CONTENT-->(.|\\n)*<!--END_CONTENT-->", "gm");
-  $.get(event.state, function(data){$(".content").html(data.match(reg));});
-};
-
 $(document).ready(function(){
     $("#back-to-top").hide();
     $(function () {
@@ -50,5 +22,12 @@ $(document).ready(function(){
         $("#back-to-comment").hide(); 
     } 
     });
-	doAtReady();
+	$(function () {
+	var res = "";
+	$("div#post-content h1, div#post-content h2, div#post-content h3, div#post-content h4, div#post-content h5, div#post-content h6").each(function(){
+		res = res + '<li><a href="#' + $(this).attr('id') + '">' + $(this).html() + '</a></li>';
+	});
+	$('#right-float').html($('#right-float').html() + '<section id="toc"><h4>ToC</h4><ul>' + res + '</ul>');
+	$('#right-float').stickUp();
+	});
 });
